58fda9e9b4448bf43a2b03935482c82095917e7f
choumanhussein <chouman.hussein7@gmail.com>
2022-03-07 20:29:48 +0000

    FacebookApp

---
+++ Gemfile
@@ -0,0 +1,45 @@
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '3.0.1'
+
+#Core
+gem 'rails', '~> 6.0.3'
+
+#Middleware
+gem 'pg', '>= 0.18', '< 2.0'
+gem 'puma', '~> 4.1'
+
+#View/Front
+gem 'sass-rails', '>= 6'
+gem 'webpacker', '~> 5.0'
+gem 'bootsnap', '>= 1.4.2', require: false
+
+#Backend
+gem 'turbolinks', '~> 5'
+gem 'jbuilder', '~> 2.7'
+gem 'bcrypt', '3.1.15'
+gem 'carrierwave', '~> 2.0'
+gem 'mini_magick'
+
+#Tool
+group :development, :test do
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-rails'
+  gem 'launchy'
+end
+
+group :development do
+  gem 'web-console', '>= 3.3.0'
+  gem 'listen', '~> 3.2'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :test do
+  gem 'capybara', '>= 2.15'
+  gem 'selenium-webdriver'
+  gem 'webdrivers'
+end
+
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,59 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+def index
+  @blogs = Blog.all.order(id: "desc")
+end
+
+def new
+  @blog = Blog.new
+end
+
+def create
+  @blog = Blog.new(blog_params)
+  @blog.user_id = current_user.id
+  if params[:back]
+    render :new
+  else
+    if @blog.save
+      redirect_to blogs_path
+    else
+      render 'new'
+    end
+  end
+end
+
+def show
+end
+
+def edit
+end
+
+def destroy
+  @blog.destroy
+  redirect_to blogs_path
+end
+
+def update
+  if @blog.update(blog_params)
+    redirect_to blogs_path
+  else
+    render :edit
+  end
+end
+
+def confirm
+  @blog = Blog.new(blog_params)
+  @blog.user_id = current_user.id
+  render :new if @blog.invalid?
+end
+
+private
+
+def blog_params
+  params.require(:blog).permit(:content, :image, :image_cache)
+end
+
+def set_blog
+  @blog = Blog.find(params[:id])
+end
+end
